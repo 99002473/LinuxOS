4a9ceadc8dcf828a12f48004d3335bdd60b54316
99002473 <bibekkumar.patro@ltts.com>
2020-10-13 10:40:00 +0530

    Code maintainance

---
+++ Activity1/src/mystrcat.c
@@ -0,0 +1,16 @@
+#include "mystring.h"
+
+void mystrcat(char str1[100], char str2[100])
+{
+    int i,j;
+    for(i=0; str1[i]!='\0'; ++i); 
+ 
+   for(j=0; str2[j]!='\0'; ++j, ++i)
+   {
+      str1[i]=str2[j];
+   }
+   
+   str1[i]='\0';
+   printf("\nOutput: %s",str1);
+   
+}
