e03cbaa8dbd1e2fb397b83a1fd078b14d9f58be2
99002473 <bibekkumar.patro@ltts.com>
2020-10-17 09:31:44 +0530

    Shell Script work push

---
+++ ShellScript/Sscript.sh
@@ -0,0 +1,36 @@
+#! /bine/bash
+
+FILE_PATH="Input.csv"
+RESULT_FILE="Results.csv"
+printf "Name,Email-ID,GitRepo-Link,Clone-Status,Build-Status,Error,Valgrind" > $RESULT_FILE
+
+while IFS=, read -r NAME ID REPO;
+do
+   [[$NAME != 'Name']] && echo "$NAME"
+   [[$ID != 'Email ID']] && echo "$ID"
+   if [ "$REPO" != 'Repo Link' ]; 
+       then
+       repo=$REPO
+       git clone "$REPO"
+       if [$? -eq 0]; 
+          then
+          clone="Clone success"
+       else
+          clone="Clone Failure"
+       fi 
+        REPO= `echo "$REPO" | cut -d'/' -c5` 
+        echo "REPO = $REPO"
+        make -C "$REPO"
+       if [$? -eq 0]; 
+           then
+           er="$ERROR"
+       fi
+        make test -C $REPO
+        EXE=`find "$REPO" -name "Test*.out"`
+        valgrind "./$EXE" 2>log.txt
+        STR = $( tail -n 1 log.txt)
+        val=$(echo ${STR:24:3})
+        verr="$val"
+        printf "$name,$id,$repo,$clone,$bu,$er,$verr\n" >> $RESULT_FILE
+   fi
+done < "${FILE PATH}"
